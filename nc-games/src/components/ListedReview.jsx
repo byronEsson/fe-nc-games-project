3e62e60d30d8aaecf065cf41d9c15456dacdcca4
byronEsson <byronesson767@gmail.com>
2022-10-31 12:13:38 +0000

    add more info to review cards

--- nc-games/src/components/ListedReview.jsx
+++ nc-games/src/components/ListedReview.jsx
@@ -1,10 +1,17 @@
 const ListedReview = ({ review }) => {
+  const dateString = `${review.created_at.slice(
+    8,
+    10
+  )}-${review.created_at.slice(5, 7)}-${review.created_at.slice(0, 4)}`;
+  console.log(review.created_at);
+  const timeString = review.created_at.slice(11, 16);
+
   return (
     <li>
       <h3>{review.title}</h3>
       <label htmlFor="designed-by" className="label">
         Designed By
-      </label>{" "}
+      </label>
       <label htmlFor="owner" className="label">
         Review By
       </label>
@@ -15,6 +22,20 @@ const ListedReview = ({ review }) => {
         alt={`image for the review ${review.title}`}
         src={review.review_img_url}
       />
+      <label htmlFor="comments" className="label bottom">
+        Comments
+      </label>
+      <label htmlFor="date" className="label bottom">
+        Posted At
+      </label>
+      <label htmlFor="votes" className="label bottom">
+        Votes
+      </label>
+      <p className="bottom">{review.comment_count}</p>{" "}
+      <p className="bottom">
+        {timeString} {dateString}
+      </p>
+      <p className="bottom">{review.votes}</p>
     </li>
   );
 };
--- nc-games/src/components/Nav.jsx
+++ nc-games/src/components/Nav.jsx
@@ -5,13 +5,24 @@ import { User } from "../contexts/User";
 const Nav = () => {
   const user = useContext(User);
 
+  const handleClick = (event) => {
+    const buttons = [...document.getElementsByClassName("nav-button")];
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+      console.log(button);
+    });
+    console.log(buttons);
+    event.target.classList.add("active");
+  };
   return (
     <nav>
       <Link to="/reviews">
-        <button className="nav-button">Reviews</button>
+        <button className="nav-button" onClick={handleClick}>
+          Reviews
+        </button>
       </Link>
       <Link to="/">
-        <button className="nav-button" id="log-out">
+        <button className="nav-button" id="log-out" onClick={handleClick}>
           Logout
         </button>
       </Link>
