8d71084a3244df2f14483d6a540ae93ee34ab967
byronEsson <byronesson767@gmail.com>
2022-11-01 13:02:57 +0000

    add sorting functionality

--- nc-games/src/api.js
+++ nc-games/src/api.js
@@ -8,6 +8,7 @@ export const fetchReviews = (query) => {
   return myApi
     .get(`/reviews${query}`)
     .then(({ data: { reviews } }) => {
+      console.log(reviews);
       return reviews;
     })
     .catch((err) => console.log(err));
